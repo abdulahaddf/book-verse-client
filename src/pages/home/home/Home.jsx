efa7324cdb3ca5ebe3ef9c1349db05fd9606b896
foisal404 <foisalahmmed2@gmail.com>
2023-08-08 15:46:34 +0600

    Category component added

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,4 +1,5 @@
-
+import Category from "../../../shared/components/Category";
+import "react-tabs/style/react-tabs.css";
 
 const Home = () => {
     return (
@@ -6,6 +7,7 @@ const Home = () => {
             {/* All Components of home will be shown here */}
             
             <h1 className="text-5xl text-center">WELCOME TO THE BOOK VERSE</h1>
+            <Category/>
 
 
             <h1>tonmoy</h1>
