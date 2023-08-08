45b1e509c027239092f394f2978ea5761236890b
zihadx <blankzihad@gmail.com>
2023-08-08 13:52:32 +0600

    another test footer

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,3 +1,4 @@
+import Footer from "../../../shared/footer/Footer";
 
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
             {/* All Components of home will be shown here */}
             
             <h1 className="text-5xl text-center">WELCOME TO THE BOOK VERSE</h1>
+            <Footer></Footer>
         </div>
     );
 };
