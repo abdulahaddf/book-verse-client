564db57b8b05111eea89781b5bed4633835cd6ba
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-08-08 13:42:44 +0600

    button

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -8,7 +8,10 @@ const Home = () => {
             <h1 className="text-5xl text-center">WELCOME TO THE BOOK VERSE</h1>
 
 
-            <h1>tonmoy</h1>
+            <h1>Tonu Tonmoy</h1>
+            <h1>Tonmoy</h1>
+
+            <button className="btn btn-neutral">Neutral</button>
         </div>
     );
 };
