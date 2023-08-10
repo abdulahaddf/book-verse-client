43114fddc91ef883854bf36eb932d6a3d2dd6efd
nahidmursaline <nahid15-12408@diu.edu.bd>
2023-08-10 12:05:51 +0600

    nahid

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   const navItems = 
    
     <>
-                <li><NavLink to="/submenu-1" activeClassName="active-link">Home</NavLink></li>
+                <li><NavLink to="/" activeClassName="active-link">Home</NavLink></li>
                 <li><NavLink to="/submenu-2" activeClassName="active-link">All Books</NavLink></li>
                 <li><NavLink to="/submenu-2" activeClassName="active-link">Popular Books</NavLink></li>
                 <li><NavLink to="/submenu-2" activeClassName="active-link">Old Books</NavLink></li>
@@ -25,9 +25,9 @@ const Navbar = () => {
     </>
 
   return (
-    <div className="navbar bg-base-100 max-w-screen-xl mx-auto">
+    <div className="navbar bg-base-100 max-w-screen-xl mx-auto ">
       <div className="navbar-start">
-        <div className="drawer lg:hidden">
+        <div className="drawer lg:hidden z-10">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
             {/* Page content here */}
