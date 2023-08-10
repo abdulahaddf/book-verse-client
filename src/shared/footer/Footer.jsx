7c2ac0ace1365e15ebfbb3f79a000d57e9e9df67
zihadx <blankzihad@gmail.com>
2023-08-11 03:28:28 +0600

    added sponsor section

--- src/shared/footer/Footer.jsx
+++ src/shared/footer/Footer.jsx
@@ -18,15 +18,22 @@ import { SiAmericanexpress } from "react-icons/si";
 import { Link } from "react-router-dom";
 import img1 from "../../assets/store/apple.png";
 import img2 from "../../assets/store/google.png";
+import logo from "../../assets/image/logo.jpg"
 const Footer = () => {
   return (
     <footer className="bg-base-200 text-base-content">
       <div className="footer p-10 flex-row md:flex-col justify-between">
         <div>
-          <h3 className="font-bold uppercase">Book Verse</h3>
-          {/* <img className="w-12 h-12 rounded-lg" src="../../assets/image/logo.jpg" alt="" /> */}
+          <div className=" flex items-center gap-4">
+            <img
+              className="w-16 h-12 rounded-sm"
+              src={logo}
+              alt=""
+            />
+            <h1 className="font-bold text-2xl text-[#d71d24]">Book verse</h1>
+          </div>
           <p>
-            Your premier source for captivating books <br /> across genres.
+            Your premier source for captivating <br /> books across genres.
             Explore with us!
           </p>
           <h3 className="font-bold uppercase my-3">Contact Us:</h3>
@@ -218,7 +225,7 @@ const Footer = () => {
             className="text-3xl text-[#016fd0] bg-white px-4 py-2 rounded-md inline-block hover:bg-[#d71d24] hover:scale-105 hover:text-white duration-1000"
             href=""
           >
-            <SiAmericanexpress></SiAmericanexpress>
+            <SiAmericanexpress className="rounded-md"></SiAmericanexpress>
           </a>
         </div>
       </div>
