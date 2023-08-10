d775adb8b43f44d3a7ed40f57eec527158b6e9a2
zihadx <blankzihad@gmail.com>
2023-08-11 04:30:56 +0600

    some problem solved

--- src/shared/footer/Footer.jsx
+++ src/shared/footer/Footer.jsx
@@ -18,18 +18,14 @@ import { SiAmericanexpress } from "react-icons/si";
 import { Link } from "react-router-dom";
 import img1 from "../../assets/store/apple.png";
 import img2 from "../../assets/store/google.png";
-import logo from "../../assets/image/logo.jpg"
+import logo from "../../assets/image/logo.jpg";
 const Footer = () => {
   return (
     <footer className="bg-base-200 text-base-content">
       <div className="footer p-10 flex-row md:flex-col justify-between">
         <div>
           <div className=" flex items-center gap-4">
-            <img
-              className="w-16 h-12 rounded-sm"
-              src={logo}
-              alt=""
-            />
+            <img className="w-16 h-12 rounded-sm" src={logo} alt="" />
             <h1 className="font-bold text-2xl text-[#d71d24]">Book verse</h1>
           </div>
           <p>
@@ -72,25 +68,25 @@ const Footer = () => {
 
         <div>
           <h3 className="font-bold uppercase">Support</h3>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Contact Customer Service</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Product Availability</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Shipping Rates</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Order Track</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1 uppercase">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative uppercase">
             <Link>Faq</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Retailer Request</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Help Center</Link>
           </p>
         </div>
@@ -98,50 +94,50 @@ const Footer = () => {
         <div>
           <h3 className="font-bold uppercase">My Account</h3>
 
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Your Account</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Your Information</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Addresses</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Orders history</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Delivery Information</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Search Terms</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Login</Link>
           </p>
         </div>
 
         <div>
           <h3 className="font-bold uppercase">Information</h3>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Terms And Condition</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Privacy Policy</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Return Policy</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Refund Policy</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Suppliers</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>Sitemap</Link>
           </p>
-          <p className="hover:bg-[#d71d24] hover:text-white hover:py-1  hover:px-1 duration-300 rounded-sm transition hover:translate-x-1">
+          <p className="hover:text-[#d71d24] duration-300 transition hover:translate-x-1 ease-in relative">
             <Link>About Us</Link>
           </p>
         </div>
@@ -156,12 +152,13 @@ const Footer = () => {
               <input
                 type="text"
                 placeholder="Your Name"
-                className="input input-bordered input-sm rounded-sm w-full max-w-xs"
+                className="input input-bordered input-sm rounded-sm max-w-xs"
               />
+              <br />
               <input
                 type="text"
                 placeholder="Your Email"
-                className="input input-bordered input-sm rounded-sm w-full my-3 max-w-xs"
+                className="input input-bordered input-sm rounded-sm my-3 max-w-xs"
               />
             </div>
             <input
