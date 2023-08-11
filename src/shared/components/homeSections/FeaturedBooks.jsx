5e8d9a11a001da7d62649a7d02774f87a774a5a8
abdulahaddf <abdulahad.df1@gmail.com>
2023-08-11 15:25:20 +0600

    merged

--- src/shared/components/homeSections/FeaturedBooks.jsx
+++ src/shared/components/homeSections/FeaturedBooks.jsx
@@ -2,24 +2,21 @@ import { Link } from "react-router-dom";
 import BookCard from "../BookCard/BookCard";
 import Heading from "../heading/Heading";
 
-
 const FeaturedBooks = () => {
-    return (
-        <div className="section">
-             <div className="flex justify-between items-center">
-            <Heading title={'Featured Books'} ></Heading>
-                <Link className="btn-primary ">See More</Link>
-               </div>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-center">
-                <BookCard/>
-                <BookCard/>
-                <BookCard/>
-                <BookCard/>
-            
-            </div>
-              
-        </div>
-    );
+  return (
+    <div className="section">
+      <div className="flex justify-between items-center">
+        <Heading title={"Featured Books"}></Heading>
+        <Link className="btn-primary ">See More</Link>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-center">
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedBooks;
