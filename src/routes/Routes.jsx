802c7c3bc8f68282073c0bc7e2de598c3bb32ae5
nhhasib <nafiul.hasan1997@gmail.com>
2023-08-16 01:51:07 +0600

    conflict resolve

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/home/home/Home";
 import AllBooks from "../pages/home/AllBooks/AllBooks";
-import BookRent from "../shared/components/BookRent/BookRent";
 export const router = createBrowserRouter([
     {
       path: "/",
@@ -15,11 +14,8 @@ export const router = createBrowserRouter([
         {
             path: "/all-books",
             element: <AllBooks/>
-        },
-        {
-          path: "/bookRent",
-          element:<BookRent></BookRent>
         }
+        
       ]
     },
   ]);
