ffd53e433ae5d3303f48850d53c087a064b5a8d3
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-08-19 23:02:11 +0600

    add img

--- src/Dashboard/Admin/AddBook.jsx
+++ src/Dashboard/Admin/AddBook.jsx
@@ -8,32 +8,141 @@ const AddBook = () => {
     register,
     handleSubmit,
     watch,
+    reset,
+  
     // eslint-disable-next-line no-unused-vars
     formState: { errors },
   } = useForm();
   console.log(watch("example"));
 
-  const onSubmit = (data) => {
-    console.log(data);
-    fetch("http://localhost:5000/allBooks", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.insertedId) {
-          Swal.fire({
-            title: "Success!",
-            text: "class added successfully",
-            icon: "success",
-            confirmButtonText: "Cool",
-          });
-        }
+
+
+    
+    // fetch("http://localhost:5000/allBooks", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(data),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.insertedId) {
+    //       Swal.fire({
+    //         title: "Success!",
+    //         text: "class added successfully",
+    //         icon: "success",
+    //         confirmButtonText: "Cool",
+    //       });
+    //     }
+    //   });
+
+  
+
+
+  // };
+
+
+// tonmoy start
+  const onSubmit = async (allData) => {
+    const {
+      title,
+      author,
+      category,
+      language,
+      real_price,
+      offer_price,
+      page_numbers,
+      rating,
+      published,
+      about_author,
+      description,
+      cover_image,
+      author_image,
+    } = allData;
+
+    const imageUploadUrl = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_Image_Upload_token}`;
+
+    try {
+      const coverForm = new FormData();
+      coverForm.append("image", cover_image[0]);
+
+      const authorForm = new FormData();
+      authorForm.append("image", author_image[0]);
+
+      // Upload Cover Image
+      const coverResponse = await fetch(imageUploadUrl, {
+        method: "POST",
+        body: coverForm,
       });
+
+      if (!coverResponse.ok) {
+        throw new Error("Cover image upload failed");
+      }
+
+      const coverImageResponse = await coverResponse.json();
+      const cover_image_url = coverImageResponse.data.display_url;
+
+      // Upload Author Image
+      const authorResponse = await fetch(imageUploadUrl, {
+        method: "POST",
+        body: authorForm,
+      });
+
+      if (!authorResponse.ok) {
+        throw new Error("Author image upload failed");
+      }
+
+      const authorImageResponse = await authorResponse.json();
+      const author_image_url = authorImageResponse.data.display_url;
+
+      // Prepare Book Data
+      const bookData = {
+        title,
+        author,
+        category,
+        language,
+        real_price,
+        offer_price,
+        page_numbers,
+        rating,
+        published,
+        about_author,
+        description,
+        cover_image: cover_image_url,
+        author_image: author_image_url,
+      };
+
+      // Send Book Data to API
+      const apiResponse = await fetch("https://book-verse-server-phi.vercel.app/allBooks", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(bookData),
+      });
+
+      if (!apiResponse.ok) {
+        throw new Error("Book insertion failed");
+      }
+
+      const responseData = await apiResponse.json();
+
+      if (responseData.insertedId) {
+        Swal.fire({
+          title: "Success!",
+          text: "Book added successfully",
+          icon: "success",
+          confirmButtonText: "Cool",
+        });
+        reset()
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
+  // tonmoy end
+  
 
   return (
     <div className="w-full h-full ps-4 md:p-4">
@@ -48,6 +157,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Book name</span>
             </label>
             <input
+            required
               type="text"
               name="title"
               {...register("title")}
@@ -60,6 +170,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Author name</span>
             </label>
             <input
+            required
               type="text"
               name="author"
               {...register("author")}
@@ -74,6 +185,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Category</span>
             </label>
             <input
+            required
               type="text"
               name="category"
               {...register("category")}
@@ -86,6 +198,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Language</span>
             </label>
             <input
+            required
               type="text"
               name="language"
               {...register("language")}
@@ -101,6 +214,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Real Prices</span>
             </label>
             <input
+            required
               type="number"
               name="real_price"
               {...register("real_price")}
@@ -114,6 +228,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Offer price*</span>
             </label>
             <input
+            required
               type="number"
               name="offer_price"
               {...register("offer_price")}
@@ -129,6 +244,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Page numbers</span>
             </label>
             <input
+            required
               type="number"
               name="page_numbers"
               {...register("page_numbers")}
@@ -142,6 +258,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Rating*</span>
             </label>
             <input
+            required
               type="text"
               name="rating"
               {...register("rating")}
@@ -157,6 +274,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">Published date</span>
             </label>
             <input
+            required
               type="date"
               name="published"
               {...register("published")}
@@ -170,6 +288,7 @@ const AddBook = () => {
               <span className="label-text font-semibold">About author</span>
             </label>
             <input
+            required
               type="rating"
               name="about_author"
               {...register("about_author")}
@@ -185,11 +304,14 @@ const AddBook = () => {
               <span className="label-text font-semibold">Cover image</span>
             </label>
             <input
-              type="text"
+            required
+              type="file"
               name="cover_image"
               {...register("cover_image")}
               placeholder="Cover image"
-              className="input input-bordered w-full"
+              className="input 
+              file-input file-input-bordered w-full "
+
             />
           </div>
           <div className="form-control w-full">
@@ -197,11 +319,12 @@ const AddBook = () => {
               <span className="label-text font-semibold">Author image</span>
             </label>
             <input
-              type="text"
+            required
+              type="file"
               name="author_image"
               {...register("author_image")}
               placeholder="Author image"
-              className="input input-bordered w-full"
+              className="input file-input file-input-bordered w-full"
             />
           </div>
         </div>
@@ -211,6 +334,7 @@ const AddBook = () => {
             <span className="label-text font-semibold">Description</span>
           </label>
           <textarea
+          required
             className="textarea textarea-bordered h-24"
             name="description"
             {...register("description")}
