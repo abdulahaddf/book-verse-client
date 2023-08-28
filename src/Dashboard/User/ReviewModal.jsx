fa462060336066f8953860e93e430e0a5698fea4
abdulahaddf <abdulahad.df1@gmail.com>
2023-08-28 17:22:25 +0600

    updated

---
+++ src/Dashboard/User/ReviewModal.jsx
@@ -0,0 +1,17 @@
+const ReviewModal = () => {
+  return (
+    <div>
+      <dialog id="my_modal_3" className="modal">
+        <form method="dialog" className="modal-box">
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </form>
+      </dialog>
+    </div>
+  );
+};
+
+export default ReviewModal;
