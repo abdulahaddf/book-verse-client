74c64d058d1305b46b2d64881891f73ddb7ea0a1
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-08-31 12:29:16 +0600

    ok

--- src/shared/MessageNotification/MessageNotification.jsx
+++ src/shared/MessageNotification/MessageNotification.jsx
@@ -58,15 +58,15 @@ const MessageNotification = ({ data, setReply, replay,buttonHandler }) => {
                         <span className="absolute inset-y-0 flex items-center">
 
                         </span>
-                        <input name="name" type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+                        <input name="name" type="text" placeholder="Write your message!" className=" focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
                             required />
                         {/* <div className="absolute right-0 items-center inset-y-0 hidden sm:flex"> */}
 
 
 
-                        <button className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none ">
+                        <button className="inline-flex items-center justify-center rounded-lg px-2 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none ">
                             
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                                 <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
                             </svg>
                         </button>
