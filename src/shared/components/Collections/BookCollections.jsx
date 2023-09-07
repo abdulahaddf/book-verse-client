85d19ad4a487fc26affca3c92e75c09524940e7f
foisal404 <foisalahmmed2@gmail.com>
2023-09-08 01:48:16 +0600

    category tab styles change

--- src/shared/components/Collections/BookCollections.jsx
+++ src/shared/components/Collections/BookCollections.jsx
@@ -1,10 +1,14 @@
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import UseBooks from "../../../hooks/UseBooks";
 import CategoryCard from "../CategoryCard";
+import { useEffect } from "react";
 
 const BookCollections = () => {
   const { books } = UseBooks();
   console.log(books);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  })
   return (
     <div className="py-5">
       <div className="mx-5 md:mx-16  p-6 bg-[#F1EDFF] rounded-md">
@@ -15,7 +19,7 @@ const BookCollections = () => {
           <style>
             {`
             .react-tabs__tab--selected{
-              background-color: #d71d24;  
+              background-color: #048ED6;  
               opacity: 90%;
               color: #fff; 
               border-radius: 10px;
