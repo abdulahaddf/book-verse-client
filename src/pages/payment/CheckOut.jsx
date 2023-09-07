4f39ea50e7a23e1a01f5206341bf1864c6f83cbb
abdulahaddf <abdulahad.df1@gmail.com>
2023-09-07 23:38:13 +0600

    checkout form

--- src/pages/payment/CheckOut.jsx
+++ src/pages/payment/CheckOut.jsx
@@ -8,32 +8,32 @@ import Swal from "sweetalert2";
 import { ImSpinner10 } from "react-icons/im";
 import { AuthContext } from "../../provider/AuthProvider";
 import useAxiosSecure from "../../hooks/UseAxiousSecure";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 const CheckOut = ({ books, price }) => {
-  console.log(price, books);
+  // console.log(price, books);
   const stripe = useStripe();
   const elements = useElements();
-  const { user,cartRefetch } = useContext(AuthContext);
+  const { user, cartRefetch } = useContext(AuthContext);
   const [axiosSecure] = useAxiosSecure();
   const [cardError, setCardError] = useState("");
   const [clientSecret, setClientSecret] = useState("");
   const [processing, setProcessing] = useState(false);
   const [paid, setPaid] = useState(false);
   const [transactionId, setTransactionId] = useState("");
+  const { getValue } = useLocalStorage();
 
   useEffect(() => {
     if (price > 0) {
       axiosSecure.post("/create-payment-intent", { price }).then((res) => {
         console.log(res.data.clientSecret);
         setClientSecret(res.data.clientSecret);
-      
-      })
-     }
+      });
+    }
   }, [price, axiosSecure]);
 
-
-
-
+  const customer = getValue("customer") // getting the customer information from the checkout form 
+  // console.log(customer);
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -76,92 +76,73 @@ const CheckOut = ({ books, price }) => {
       console.log(confirmError);
       setCardError(confirmError.message);
     }
-setTransactionId(paymentIntent.id)
+    setTransactionId(paymentIntent.id);
     console.log("payment intent", paymentIntent);
 
     if (paymentIntent.status === "succeeded") {
       // save payment information to the server
+      
       const paymentInfo = {
         books: [...books],
         transactionId: paymentIntent.id,
         mail: user?.email,
         date: new Date(),
-        name: user?.displayName ,
-        total_price: price
+        total_price: price,
+        customer,
       };
       axiosSecure.post("/payments", paymentInfo).then((res) => {
         console.log(res.data);
         if (res.data.insertedId) {
           setProcessing(false);
 
-            // Post bestSelling && recentSelling  start by Tonmoy
+          // Post bestSelling && recentSelling  start by Tonmoy
 
-         
-            const array=[]
-    
-    
-            for(let i of books){
-            
-            
-                const a={
-            
-                  about_author: i?.about_author,
-                  author:i?.author,
-                  author_image:i?.author_image,
-                  category:i?.category,
-                  count:i?.count ||1,
-                  cover_image:i?.cover_image,
-                  description:i?.description,
-                  language:i?.language,
-                  offer_price:i?.offer_price,
-                  page_numbers:i?.page_numbers,
-                  published:i?.published,
-                  rating:i?.rating,
-                  real_price:i?.real_price,
-                  review:i?.review,
-                  title:i?.title,
-                  previous_id:i?._id,
-                  purchase_date: new Date().getTime(),
-          
-            
-            
-            
-                  }
-                 
-              array.push(a)
-            
-              console.log(i)
-            }
-            
-            
-            console.log(array)
-          
-          
-          
-              if(array){
-          
-                for(let a of array){
-          
-          
-                  fetch('https://book-verse-server-phi.vercel.app/bestSellingAndRecentSelling',{
-                    method:'POST',
-                    headers:{
-                      'content-type':'application/json'
-                    },
-                    body:JSON.stringify(a)
-                  })
-                  
-                  
-          
+          const array = [];
+
+          for (let i of books) {
+            const a = {
+              about_author: i?.about_author,
+              author: i?.author,
+              author_image: i?.author_image,
+              category: i?.category,
+              count: i?.count || 1,
+              cover_image: i?.cover_image,
+              description: i?.description,
+              language: i?.language,
+              offer_price: i?.offer_price,
+              page_numbers: i?.page_numbers,
+              published: i?.published,
+              rating: i?.rating,
+              real_price: i?.real_price,
+              review: i?.review,
+              title: i?.title,
+              previous_id: i?._id,
+              purchase_date: new Date().getTime(),
+            };
+
+            array.push(a);
+
+            console.log(i);
+          }
+
+          console.log(array);
+
+          if (array) {
+            for (let a of array) {
+              fetch(
+                "https://book-verse-server-phi.vercel.app/bestSellingAndRecentSelling",
+                {
+                  method: "POST",
+                  headers: {
+                    "content-type": "application/json",
+                  },
+                  body: JSON.stringify(a),
                 }
-          
-          
-              }
-              
-             
+              );
+            }
+          }
 
-            // Post bestSelling && recentSelling  end by Tonmoy
-
+          // Post bestSelling && recentSelling  end by Tonmoy
 
           Swal.fire({
             position: "top-end",
@@ -173,7 +154,7 @@ setTransactionId(paymentIntent.id)
           localStorage.removeItem("cartItems");
           localStorage.removeItem("totalPrice");
           setPaid(true);
-          cartRefetch()
+          cartRefetch();
         }
       });
     }
@@ -184,26 +165,29 @@ setTransactionId(paymentIntent.id)
       {paid ? (
         <div className="md:my-40">
           <h1 className="text-3xl text-center mt-10">
-            Congratulations!! You have Paid For All Your Books{" "}  
+            Congratulations!! You have Paid For All Your Books{" "}
           </h1>
-          <h3 className="text-xl text-center my-5">Your Transaction Id : <span className="text-red">{transactionId}</span></h3>
+          <h3 className="text-xl text-center my-5">
+            Your Transaction Id :{" "}
+            <span className="text-red">{transactionId}</span>
+          </h3>
           <h2 className="text-2xl text-center mt-5">
             Please wait for the fastest delivery
           </h2>
         </div>
       ) : (
         <>
-         
           <form
             className="w-1/2 p-20 mt-20 mx-auto text-white border-double border-4 border-red bg-black"
             onSubmit={handleSubmit}
           >
             <h1 className="text-center">Hello, {user?.displayName} </h1>
             <h1 className="text-3xl text-center my-10">
-            You Need to Pay{" "}
-            <span className="font-semibold text-red">${price}</span> for{" "}
-            <span className="font-semibold text-red">{books.length}</span> Books
-          </h1>
+              You Need to Pay{" "}
+              <span className="font-semibold text-red">${price}</span> for{" "}
+              <span className="font-semibold text-red">{books.length}</span>{" "}
+              Books
+            </h1>
             <CardElement
               options={{
                 style: {
@@ -223,7 +207,7 @@ setTransactionId(paymentIntent.id)
             <button
               type="submit"
               // disabled={!stripe || !clientSecret || processing}
-              className="inline-flex justify-center rounded-md border border-transparent bg-red px-4 py-2 text-sm font-medium text-white hover:bg-[#ef4444] focus:outline-none focus-visible:ring-2 focus-visible:ring-cyan-500 focus-visible:ring-offset-2"
+              className="inline-flex justify-center rounded-md border border-transparent bg-red px-4 py-2 text-sm font-medium text-white hover:bg-secondary focus:outline-none focus-visible:ring-2 focus-visible:ring-cyan-500 focus-visible:ring-offset-2"
             >
               {processing ? (
                 <ImSpinner10 className="m-auto animate-spin" size={24} />
@@ -235,11 +219,7 @@ setTransactionId(paymentIntent.id)
               {cardError && <p className="text-red-600 ">{cardError}</p>}
             </div>
           </form>
-
-          
         </>
-
-      
       )}
     </div>
   );
